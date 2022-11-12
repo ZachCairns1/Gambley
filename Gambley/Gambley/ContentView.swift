e8a61db9a2e324553b36e8048f34a57342c4e0f7
Zachary Cairns <zcairns@email.unc.edu>
2022-11-12 17:39:44 -0500

    Game is functional.

--- Gambley/Gambley/ContentView.swift
+++ Gambley/Gambley/ContentView.swift
@@ -9,21 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var name: String = ""
-    @State var current = ""
-    @State var money: Int = 100
-    @State var reset = false
-    @State var submitted = false
-    
-    @State var rolls: Int = 0
-    @State var computerRolls: [Int] = []
-    @State var yourRolls: [Int] = []
-    
-    @State var computerRoll = 0
-    @State var yourRoll = 0
-    
-    @State var compSum = 0
-    @State var yourSum = 0
+    @StateObject var vm = GambleyViewModel()
     
     var body: some View {
         VStack{
@@ -31,32 +17,44 @@ struct ContentView: View {
                 .font(.system(size: 36, weight: .black, design: .serif))
             NavigationStack {
                 VStack {
-                    if(!submitted) {
+                    if(!vm.submitted) {
                         Form{
                             Text("**Rules:**")
-                            Text("1. Place your wager. ")
-                            Text("2. Select number of dice to roll")
-                            Text("3. Roll your dice.")
+                            Text("1. Select number of dice to roll.")
+                            Text("2. Roll your dice and beat the computer!")
+                            Text("3. Place your wager. ")
                             HStack{
                                 Text("Enter name: ")
-                                TextField("Enter your name", text: $current)
+                                TextField("Enter your name", text: $vm.current)
                                     .border(.secondary)
                                     .autocorrectionDisabled()
                                     .onSubmit {
-                                        name = current
+                                        vm.name = vm.current
                                     }
                             }
                             VStack{
                                 Text("Choose number rolls")
-                                Picker("Rolls", selection: $rolls) {
-                                    ForEach(1...10, id: \.self) { number in
+                                Picker("Rolls", selection: $vm.rolls) {
+                                    ForEach(0...10, id: \.self) { number in
                                         Text("\(number)")
                                     }
                                 }
                                 .pickerStyle(WheelPickerStyle())
                             }
+                            HStack{
+                                Text("Enter wager: ")
+                                TextField("Enter your score", value: $vm.tempWager, format: .number)
+                                    .border(.secondary)
+                                    .onSubmit {
+                                        vm.wager = vm.tempWager
+                                    }
+                            }
                             Button {
-                                submitted = true
+                                if(vm.wager > 0 && vm.name != "" && vm.rolls > 0){
+                                    if (vm.wager <= vm.money){
+                                        vm.submitted = true
+                                    }
+                                }
                             } label: {
                                 Text("                               ROLL").bold()
                             }
@@ -64,61 +62,128 @@ struct ContentView: View {
                         
                     }
                     else {
-                        Text("Rolls left: \(rolls)")
-                        Button {
-                            if(rolls > 0) {
-                                computerRoll = Int.random(in: 1..<7)
-                                computerRolls.append(computerRoll)
-                                compSum += computerRoll
-                                
-                                yourRoll = Int.random(in: 1..<7)
-                                yourRolls.append(yourRoll)
-                                yourSum += yourRoll
-                                
-                                rolls -= 1
+                        ZStack{
+                            VStack{
+                                Text("Rolls left: \(vm.rolls)")
+                                Button {
+                                    if(vm.rolls > 0 && vm.rolls != 1) {
+                                        vm.rollDie()
+                                    }
+                                    else if(vm.rolls == 1) {
+                                        vm.rollDie()
+                                        if (vm.compSum >= vm.yourSum) {
+                                            //You lose lol.
+                                            vm.money -= vm.wager
+                                        }
+                                        else{
+                                            vm.money += vm.wager
+                                        }
+                                    }
+                                } label: {
+                                    Text("Roll")
+                                }
+                                Spacer()
+                                HStack{
+                                    List {
+                                        Section {
+                                            Text("**Comp score: \(vm.compSum)**")
+                                                .font(.system(size: 18))
+                                            ForEach(vm.computerRolls, id: \.self) { task in
+                                                Text("Roll: \(task)")
+                                            }
+                                        }
+                                    }
+                                    .frame(maxHeight: 550)
+                                    List {
+                                        Section {
+                                            Text("**Your score: \(vm.yourSum)**")
+                                                .font(.system(size: 18))
+                                            ForEach(vm.yourRolls, id: \.self) { task in
+                                                Text("Roll: \(task)")
+                                            }
+                                        }
+                                    }
+                                    .frame(maxHeight: 550)
+                                    
+                                }
                             }
-                        } label: {
-                            Text("Roll")
+                            if(vm.rolls == 0) {
+                                Rectangle().fill(Color.black)
+                                    .frame(width: 300, height: 300)
+                                if (vm.compSum >= vm.yourSum) {
+                                    VStack {
+                                        Text("**Computer: \(vm.compSum), You: \(vm.yourSum)**")
+                                            .foregroundColor(.white)
+                                            .font(.system(size: 30.0))
+                                            .padding(.bottom, 30)
+                                        Text("You Lost $\(vm.wager)")
+                                            .foregroundColor(.red)
+                                            .font(.system(size: 30.0))
+                                            .padding(.bottom, 30)
+                                        Text("Your Updated Total: $\(vm.money)")
+                                            .foregroundColor(.red)
+                                            .font(.system(size: 24.0))
+                                            .padding(.bottom, 30)
+                                        Button {
+                                            vm.submitted = false
+                                            vm.tempWager = 0
+                                            vm.compSum = 0
+                                            vm.yourSum = 0
+                                            vm.wager = 0
+                                            vm.yourRolls.removeAll()
+                                            vm.computerRolls.removeAll()
+                                        } label: {
+                                            Text("Play Again?")
+                                                .foregroundColor(.blue)
+                                                .font(.system(size: 24.0))
+                                        }
+                                    }
+                                } else {
+                                    VStack {
+                                        Text("**Computer \(vm.compSum), You: \(vm.yourSum)**")
+                                            .foregroundColor(.white)
+                                            .font(.system(size: 30.0))
+                                            .padding(.bottom, 30)
+                                        Text("You Won $\(vm.wager)!!!")
+                                            .foregroundColor(.green)
+                                            .font(.system(size: 24.0))
+                                            .padding(.bottom, 30)
+                                        Text("Your Updated Total: $\(vm.money)")
+                                            .foregroundColor(.green)
+                                            .font(.system(size: 24.0))
+                                            .padding(.bottom, 30)
+                                        Button {
+                                            vm.submitted = false
+                                            vm.tempWager = 0
+                                            vm.compSum = 0
+                                            vm.yourSum = 0
+                                            vm.wager = 0
+                                            vm.yourRolls.removeAll()
+                                            vm.computerRolls.removeAll()
+                                        } label: {
+                                            Text("Play Again?")
+                                                .foregroundColor(.blue)
+                                                .font(.system(size: 24.0))
+                                        }
+                                    }
+                                }
+                                
+                            }
                         }
-                        Spacer()
-                        HStack{
-                            List {
-                                Section {
-                                    Text("Comp score: \(compSum)")
-                                    ForEach(computerRolls, id: \.self) { task in
-                                        Text("Roll: \(task):")
-                                    }
-                                }
-                            }
-                            .frame(maxHeight: 550)
-                            List {
-                                Section {
-                                    Text("Your score: \(yourSum)")
-                                    ForEach(yourRolls, id: \.self) { task in
-                                        Text("Roll: \(task)")
-                                    }
-                                }
-                            }
-                            .frame(maxHeight: 550)
                             
-                        }
-                        Text("HAHAHAHHAAHHA")
+                        
                     }
                     HStack{
-                        Text("Player: " + name)
-                            .padding(.trailing, 20)
-                        Text("Money: $\(money)")
-                            .padding(.trailing, 50)
-                        Button("reset", action: {
-                            reset = true
-                            submitted = false
-                            money = 100
-                            name = ""
-                            rolls = 0
-                            computerRolls.removeAll()
-                            yourRolls.removeAll()
-                            compSum = 0
-                            yourSum = 0
+                        Text("Player: " + vm.name)
+                            .foregroundColor(.red)
+                            .padding(.trailing, 0)
+                        Text("Money: $\(vm.money)")
+                            .padding(.trailing, 0)
+                            .foregroundColor(.green)
+                        Text("Current Wager: \(vm.wager)")
+                            .foregroundColor(.purple)
+                        Button("End game", action: {
+                            vm.resetGame()
                         })
                     }
                 }
@@ -139,3 +204,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- Gambley/Gambley/GambleyViewModel.swift
+++ Gambley/Gambley/GambleyViewModel.swift
@@ -7,6 +7,58 @@
 
 import SwiftUI
 
-class GambleyViewModel {
+class GambleyViewModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var current = ""
+    @Published var money: Int = 100
+    @Published var reset = false
+    @Published var submitted = false
     
+    @Published var rolls: Int = 0
+    @Published var computerRolls: [Int] = []
+    @Published var yourRolls: [Int] = []
+    
+    @Published var computerRoll = 0
+    @Published var yourRoll = 0
+    
+    @Published var compSum = 0
+    @Published var yourSum = 0
+    
+    @Published var wager = 0
+    @Published var tempWager = 0
+    
+    
+    
+    func rollDie() {
+        if(rolls > 0) {
+            computerRoll = Int.random(in: 1..<7)
+            computerRolls.append(computerRoll)
+            compSum += computerRoll
+            
+            yourRoll = Int.random(in: 1..<7)
+            yourRolls.append(yourRoll)
+            yourSum += yourRoll
+            
+            rolls -= 1
+        }
+    }
+    
+    func resetGame() {
+        reset = true
+        submitted = false
+        money = 100
+        name = ""
+        rolls = 0
+        computerRolls.removeAll()
+        yourRolls.removeAll()
+        compSum = 0
+        yourSum = 0
+        wager = 0
+        current = ""
+        tempWager = 0
+    }
+    
+    func payOut(){
+        
+    }
 }
